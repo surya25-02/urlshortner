62dda83cfa0b2a533326c69560ebeaf7bc902125
Surya <47964646+surya25-02@users.noreply.github.com>
2024-04-11 12:20:06 +0530

    re-short button behavior and link existence check
    - Fixed the behavior of the re-short button to properly handle re-shortening URLs.
    - Implemented a check to return the existing link if it already exists in the database.

--- shortner/static/shortner/js/function.js
+++ shortner/static/shortner/js/function.js
@@ -1,20 +1,18 @@
-document.getElementById('username')?.addEventListener('input', check1);
-document.getElementById('password')?.addEventListener('input', check1);
-document.getElementById('confirmpassword')?.addEventListener('input', check1);
+document.getElementById('username')?.addEventListener('input', signup_validation);
+document.getElementById('password')?.addEventListener('input', signup_validation);
+document.getElementById('confirmpassword')?.addEventListener('input', signup_validation);
 document.getElementById('shorten')?.addEventListener('click', shortlink);
 document.querySelectorAll('#removebutton').forEach(i => {
-    i.addEventListener('click', remove_shortLink);
+    i.addEventListener('click', remove_shortlink);
 })
 
 
 async function sleep(seconds) {
-    return await new Promise( reslove => {
-        setTimeout(reslove, seconds * 1000)
-    })
+    return new Promise(reslove => setTimeout(reslove, seconds * 1000));
 }
 
 window.onload = function winload() {
-    
+
     tippy('[data-tippy-content]', {
         hideOnClick: false,
         theme: 'black',
@@ -28,94 +26,93 @@ window.onload = function winload() {
             })
         }
     });
-    let title = document.title;
-    body = document.getElementsByTagName('body');
-    if (['Login Page', 'SignUp', ].includes(title)) {
-        body[0].style.backgroundColor = '#1d2a35'        
-    } 
+
+    const [body] = document.getElementsByTagName('body');
+
+    if (['Login Page', 'SignUp',].includes(document.title)) {
+        body.style.backgroundColor = '#1d2a35'
+    }
 }
 
 function copy_text_to_clipboard(element) {
-
-    let text = element.getAttribute('data-clipboard-target')
+    const text = element.getAttribute('data-clipboard-target')
     navigator.clipboard.writeText(text)
-    
 }
 
-function check1() {
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    let cpassword = document.getElementById('confirmpassword').value;
-    let button = document.getElementById('formbutton');
-    let disabled = false;
-    if (/[^A-Z0-9_]/i.test(username.trim())) {
+async function signup_validation() {
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value;
+    const cpassword = document.getElementById('confirmpassword').value;
+    const button = document.getElementById('formbutton');
+    button.disabled = true;
+
+    if (/[^A-Z0-9_]/i.test(username)) {
         document.getElementById('usernamehelptext').classList.remove('d-none');
-        button.disabled = true;
-        disabled = true;
     } else {
         document.getElementById('usernamehelptext').classList.add('d-none');
-        button.disabled = false;
-        disabled = false;
-    }
-    if (password.length > 0 && cpassword.length > 0 && password != cpassword) {
-        document.getElementById('passwordhelptext').classList.remove('d-none');
-        button.disabled = true;
-    } else {
-        document.getElementById('passwordhelptext').classList.add('d-none');
-        if (!disabled) button.disabled = false;
     }
 
+    if (password !== cpassword) {
+        document.getElementById('passwordhelptext').classList.remove('d-none');
+    } else {
+        document.getElementById('passwordhelptext').classList.add('d-none');
+        if (!/[^A-Z0-9_]/i.test(username)) {
+            button.disabled = false;
+        }
+    }
 }
 
 async function showpassword() {
-    let password = document.querySelector('[name=password]');
-    let cpassword = document.getElementById('confirmpassword');
-    if (password) {
-        password.type == 'password' ? password.type = 'text' : password.type = 'password';
-        let icon = document.getElementById('eye');
-        if (icon) {
-            icon.className == "fa-solid fa-eye-slash" ? icon.className = "fa-solid fa-eye" : icon.className = "fa-solid fa-eye-slash";
-        }
-    }
-    if (cpassword) {
-        cpassword.type == 'password' ? cpassword.type = 'text' : cpassword.type = 'password';
+    const password = document.querySelector('[name=password]');
+    password.type == 'password' ? password.type = 'text' : password.type = 'password';
+
+    if (document.title === 'Login Page') {
+        const icon = document.getElementById('eye');
+        icon.className = icon.className === "fa-solid fa-eye-slash" ? "fa-solid fa-eye" : "fa-solid fa-eye-slash";
+    } else {
+        const cpassword = document.getElementById('confirmpassword');
+        cpassword.type = cpassword.type === 'password' ? 'text' : 'password';
     }
 }
 
 async function shortlink() {
-    let button = this;
-    button.classList.add('clicked')
-    button.innerHTML = `<i class="fa-solid fa-rotate fa-spin fa-lg"></i>`;
-    
-    let url = document.getElementById('url');
+    const url_input_box = document.getElementById('url_input_box');
+    const long_url = url_input_box.value;
+
+    if (!long_url) {
+        alert('Enter the URL first')
+        return;
+    }
+
+    this.classList.add('clicked')
+    this.innerHTML = `<i class="fa-solid fa-rotate fa-spin fa-lg"></i>`;
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    let form = new FormData();
-    form.append('long_url', url.value)
+    const form = new FormData();
+    form.append('long_url', long_url)
     form.append('csrfmiddlewaretoken', csrftoken)
-    let res = await fetch('/api/shortlink', {
+
+    const res = await fetch('/api/shortlink', {
         method: 'POST',
         body: form,
-    });
+    })
+
     if ([401, 403].includes(res.status)) {
         return window.location.href = '/login'
     }
-    res = await res.json()
 
-    button.innerHTML = `<i class="fa-solid fa-check"></i>`;
+    const { shorten_link } = await res.json();
+    this.innerHTML = `<i class="fa-solid fa-check"></i>`;
     await sleep(1)
-
-    let inputgroup = document.getElementById('change');
-
-    let link = res.shorten_link
+    const inputgroup = document.getElementById('change');
 
     if (document.title === 'Dashboared') {
-        url.value = link
-        button.innerHTML = 'SHORT'
-        return
+        url_input_box.value = shorten_link;
+        this.innerHTML = 'SHORT';
+        return;
     }
-   
-    inputgroup.innerHTML = `<input type="text" class="form-control p-3 border-0" id="url" value="${link}" style="box-shadow: none;" readonly>
-    <button class="btn btn-light" type="button" id="copy" data-clipboard-target="${link}"><i class="fa-regular fa-copy"></i></button>
+
+    inputgroup.innerHTML = `<input type="text" class="form-control p-3 border-0" id="url_input_box" value="${shorten_link}" style="box-shadow: none;" readonly>
+    <button class="btn btn-light" type="button" id="copy" data-clipboard-target="${shorten_link}"><i class="fa-regular fa-copy"></i></button>
     <button class="btn btn-light" type="button" id="reshort"><i class="fa-solid fa-rotate"></i></button>
     `
 
@@ -134,7 +131,7 @@ async function shortlink() {
             })
         }
     })
-    
+
 
     tippy('#reshort', {
         theme: 'black',
@@ -144,8 +141,9 @@ async function shortlink() {
         onShow(instance) {
             instance.reference.addEventListener('click', async () => {
                 inputgroup.innerHTML = `
-                <input type="text" class="form-control p-3 border-0" id="url" placeholder="your url here" style="box-shadow: none;" required>
+                <input type="text" class="form-control p-3 border-0" id="url_input_box" placeholder="your url here" style="box-shadow: none;" required>
                 <button class="btn btn-light fw-bold" type="submit" id="shorten">Shorten</button>`;
+                document.getElementById('shorten')?.addEventListener('click', shortlink)
                 await sleep(1)
                 instance.hide();
             })
@@ -154,14 +152,14 @@ async function shortlink() {
 
 }
 
-async function remove_shortLink(){
-    let button = this;
-    var newElement = document.createElement("div");
+async function remove_shortlink() {
+    const newElement = document.createElement("div");
     newElement.className = "spinner-border text-danger"
     newElement.role = "status"
-    button.parentNode.replaceChild(newElement, button);
+    this.parentNode.replaceChild(newElement, this);
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    res = await fetch(`${location.origin}/api/shortlink?url=${button.value}`,
+
+    const res = await fetch(`/api/shortlink?url=${this.value}`,
         {
             method: 'DELETE',
             headers: {
@@ -169,7 +167,8 @@ async function remove_shortLink(){
             },
         }
     )
-    if (res.status == 204){
+
+    if (res.status === 204) {
         location.reload(true);
     }
 }
--- shortner/views.py
+++ shortner/views.py
@@ -31,7 +31,7 @@ class HomeView(View):
 class UserLogout(View):
     def get(self, request):
         logout(request)
-        return redirect('home')
+        return redirect("home")
 
 
 class UserLogin(View):
@@ -43,17 +43,21 @@ class UserLogin(View):
         return render(request, self.template_name)
 
     def post(self, request):
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST.get("username")
+        password = request.POST.get("password")
         if user := authenticate(username=username, password=password):
             login(request, user)
             return redirect("home")
         else:
-            return render(request, self.template_name, {"error": True, "username": username, "password": password})
+            return render(
+                request,
+                self.template_name,
+                {"error": True, "username": username, "password": password},
+            )
 
 
 class UserSignup(View):
-    template_name = 'shortner/signup.html'
+    template_name = "shortner/signup.html"
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -64,105 +68,130 @@ class UserSignup(View):
         if request.user.is_authenticated:
             return redirect("home")
 
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        email = request.POST.get('email')
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
 
         if authenticate(username=username, password=password):
-            return render(request, self.template_name, {'error': True, 'text': 'You Already Have an Account, Please Login.'})
+            return render(
+                request,
+                self.template_name,
+                {"error": True, "text": "You Already Have an Account, Please Login."},
+            )
         elif User.objects.filter(username=username).exists():
-            return render(request, self.template_name, {'error': True, 'text': 'Username is already taken.'})
+            return render(
+                request,
+                self.template_name,
+                {"error": True, "text": "Username is already taken."},
+            )
         elif User.objects.filter(email=email).exists():
-            return render(request, self.template_name, {'error': True, 'text': 'Email is already taken.'})
+            return render(
+                request,
+                self.template_name,
+                {"error": True, "text": "Email is already taken."},
+            )
 
         user = User.objects.create_user(
-            username=username, password=password, email=email)
-        login(request, user)
+            username=username, password=password, email=email
+        )
+        UserStatistics.objects.create(user=user)
         Token.objects.get_or_create(user=user)
-        return redirect('home')
+        login(request, user)
+
+        return redirect("home")
 
 
 class GetShortLink(View):
     template_name = "shortner/404.html"
 
     def get(self, request, short_code=None):
-        if short_code and Link.objects.filter(short_code=short_code).exists():
-            ob = Link.objects.get(short_code=short_code)
-            ob.total_views += 1
-            ob.save()
-            Statistics, created = UserStatistics.objects.get_or_create(
-                user=request.user)
-            Statistics.total_month_views += 1
-            Statistics.total_today_views += 1
-            Statistics.save()
-            return redirect(ob.long_url)
-        else:
+        if not short_code:
             return render(request, self.template_name, status=404)
+
+        link = Link.objects.filter(short_code=short_code)
+
+        if link.exists():
+            link = link.get()
+            link.total_views += 1
+            link.save()
+            statistics = UserStatistics.objects.get(user=link.user)
+            statistics.total_month_views += 1
+            statistics.total_today_views += 1
+            statistics.save()
+
+            return redirect(link.long_url)
 
 
 class DashboardView(LoginRequiredMixin, ListView):
     model = Link
-    template_name = 'shortner/dashboard.html'
+    template_name = "shortner/dashboard.html"
     paginate_by = 10
 
     def get_queryset(self):
-        return self.model.objects.filter(user=self.request.user).order_by('-created_date')
+        return self.model.objects.filter(user=self.request.user).order_by(
+            "-created_date"
+        )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        Statistics, created = UserStatistics.objects.get_or_create(
-            user=self.request.user)
-        context['Statistics'] = Statistics
+        statistics = UserStatistics.objects.get(user=self.request.user)
+        context["Statistics"] = statistics
         return context
 
 
 class URLShortenerAPI(APIView):
     model = Link
     parser_classes = [JSONParser, MultiPartParser]
-    authentication_classes = [TokenAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
 
     @extend_schema(
-        summary='Short Link',
+        summary="Short Link",
         description="Create Link",
         request=LinkSerializer,
         responses={
-            200: OpenApiResponse(description='Json Response'),
-            400: OpenApiResponse(description='Validation error')
-        }
+            200: OpenApiResponse(description="Json Response"),
+            400: OpenApiResponse(description="Validation error"),
+        },
     )
     def post(self, request):
-        data = request.data
-        serializer = LinkSerializer(data=data)
-        if serializer.is_valid():
-            long_url = serializer.validated_data.get('long_url')
-            short_code = shortcode_generator()
-            Link.objects.create(long_url=long_url,
-                            short_code=short_code, user=request.user)
-            return Response({'shorten_link': f'https://{ request.get_host() }/{short_code}'}, status=status.HTTP_201_CREATED)
-        else:
+        serializer = LinkSerializer(data=request.data)
+
+        if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+        long_url = serializer.validated_data.get("long_url")
+        link, created = Link.objects.get_or_create(
+            long_url=long_url,
+            user=request.user,
+            defaults={"short_code": shortcode_generator()},
+        )
+
+        return Response(
+            {"shorten_link": f"https://{ request.get_host() }/{link.short_code}"},
+            status=status.HTTP_201_CREATED,
+        )
+
     @extend_schema(
-        summary='Delete Link',
+        summary="Delete Link",
         description="Delete Link",
         request=LinkSerializer,
         parameters=[
             OpenApiParameter(
-                name='url',
-                description='Enter Shorten Url',
+                name="url",
+                description="Enter Shorten Url",
                 type=str,
-                location=OpenApiParameter.QUERY
+                location=OpenApiParameter.QUERY,
             ),
         ],
         responses={
-            204: OpenApiResponse(description='No Content'),
-            400: OpenApiResponse(description='Validation error'),
-            404: OpenApiResponse(description='Not Found')
-        }
+            204: OpenApiResponse(description="No Content"),
+            400: OpenApiResponse(description="Validation error"),
+            404: OpenApiResponse(description="Not Found"),
+        },
     )
     def delete(self, request):
-        short_code = request.GET.get('url').split('/')[-1]
-        ob = get_object_or_404(self.model, short_code=short_code)
-        ob.delete()
+        short_code = request.GET.get("url").split("/")[-1]
+        link = get_object_or_404(self.model, short_code=short_code, user=request.user)
+        link.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
